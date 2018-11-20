8c92f753a2d8d8a70ab09d928c90b8cead6ea17e
grassknoted <grassknoted@gmail.com>
2018-11-20 22:10:27 +0530

    server changes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('MathViz running on port 3000!')
 })
