97785609bc61095c2fc8e9ecbf5c8f67c3339f55
grassknoted <grassknoted@gmail.com>
2018-11-21 17:33:08 +0530

    added packages cors and mysql

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
+const cors = require('cors')
+
+var corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
+
+app.use(cors(corsOptions))
+
 
 vision_api_key = "************************************";
+
 
 app.get('/', function (req, res) {
   res.render("index")
@@ -20,6 +30,15 @@ app.post('/', function (req, res) {
 app.listen(3000, function () {
   console.log('MathViz running on port 3000!')
 })
+
+app.route('/api/cats/:name').get((req, res) => {
+  const requestedCatName = req.params['name'];
+  res.send({ name: requestedCatName });
+});
+
+app.route('/api/cats').post((req, res) => {
+  res.send(201, req.body);
+});
 
 /*let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
