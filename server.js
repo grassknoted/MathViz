5559ab57b71425e97828e8b8312d8da1fbf00306
grassknoted <grassknoted@gmail.com>
2018-11-21 12:14:57 +0530

    structuring

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render("index")
 })
+
+app.post('/', function (req, res) {
+    res.render('index');
+    console.log.apply(req.body.city);
+  })
 
 app.listen(3000, function () {
   console.log('MathViz running on port 3000!')
 })
+
+/*let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+*/
