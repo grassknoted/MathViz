dcd237d63da1b595cb7097508c36005b353369db
grassknoted <grassknoted@gmail.com>
2018-11-23 09:05:36 +0530

    styling changes

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,20 @@ math_sign = document.getElementById('math_sign');
 math_sign_alt = 0;
 setInterval(
     function() { 
-        if(math_sign_alt) {
-            math_sign.innerHTML = "÷";
-            math_sign_alt = 0;
-        }
-        else {
+        if(math_sign_alt == 0) {
             math_sign.innerHTML = "+";
             math_sign_alt = 1;
+        }
+        else if(math_sign_alt == 1) {
+            math_sign.innerHTML = "−";
+            math_sign_alt = 2;
+        }
+        else if(math_sign_alt == 2) {
+            math_sign.innerHTML = "×";
+            math_sign_alt = 3;
+        }
+        else if(math_sign_alt == 3) {
+            math_sign.innerHTML = "÷";
+            math_sign_alt = 0;
         }
     }, 1000);
