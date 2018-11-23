aeab1d958ec780bad840c37c0c6e2dff0e8fb448
grassknoted <grassknoted@gmail.com>
2018-11-23 19:34:27 +0530

    quadratic solver added

--- public/js/equation.js
+++ public/js/equation.js
@@ -1,25 +1,135 @@
-function solve() {
-    alert("solver called");
-    var a = document.getElementById('a');
-    var b = document.getElementById('b');
-    var m = document.getElementById('m');
+function simultaneous_solve() {
+    console.log("Solving the Simultaneous Equation!");
 
-    var c = document.getElementById('c');
-    var d = document.getElementById('d');
-    var n = document.getElementById('n');
+    var a_var = document.getElementById('a');
+    var b_var = document.getElementById('b');
+    var m_var = document.getElementById('m');
 
-    a = parseFloat(a.value);
-    b = parseFloat(b.value);
-    m = parseFloat(m.value);
+    var c_var = document.getElementById('c');
+    var d_var = document.getElementById('d');
+    var n_var = document.getElementById('n');
 
-    c = parseFloat(c.value);
-    d = parseFloat(d.value);
-    n = parseFloat(n.value);
+    var a_value = parseFloat(a_var.value);
+    var b_value = parseFloat(b_var.value);
+    var m_value = parseFloat(m_var.value);
 
-    var x = (n*b - m*d)/(b*c - a*d);
-    var y = (m*c - n*a)/(b*c - a*d);    
+    var c_value = parseFloat(c_var.value);
+    var d_value = parseFloat(d_var.value);
+    var n_value = parseFloat(n_var.value);
 
-    var answer = document.getElementById("equation_answer");
+    console.log(a_value);
+    console.log(b_value);
+    console.log(m_value);
+
+    console.log(c_value);
+    console.log(d_value);
+    console.log(n_value);
+
+    console.log("x = "+((n_value*b_value - m_value*d_value)/(b_value*c_value - a_value*d_value)));
+    console.log("y = "+((m_value*c_value - n_value*a_value)/(b_value*c_value - a_value*d_value)));
+
+    var x = ((n_value*b_value - m_value*d_value)/(b_value*c_value - a_value*d_value));
+    var y = ((m_value*c_value - n_value*a_value)/(b_value*c_value - a_value*d_value));    
+
+    var answer = document.querySelector(".text");
 
     answer.innerHTML = "x="+x+" y="+y;
 }
+
+function quadratic_solve() {
+    console.log("Solving the Quadratic Equation!");
+    var a_var = document.getElementById('a_quad');
+    var b_var = document.getElementById('b_quad');
+    var c_var = document.getElementById('c_quad');
+
+    var a_value = parseFloat(a_var.value);
+    var b_value = parseFloat(b_var.value);
+    var c_value = parseFloat(c_var.value);
+
+    var x1 = (-b_value + Math.sqrt(Math.pow(b_value, 2) - (4*a_value*c_value)))/(2*a_value);
+    var x2 = (-b_value - Math.sqrt(Math.pow(b_value, 2) - (4*a_value*c_value)))/(2*a_value);
+
+    if(x1 == x2) {
+        console.log(x1);
+    }
+    else {
+        console.log(x1+" "+x2);
+    }
+}
+
+// class TextScramble {
+//     constructor(el) {
+//       this.el = el
+//       this.chars = '!<>-_\\/[]{}—=+*^?#________'
+//       this.update = this.update.bind(this)
+//     }
+//     setText(newText) {
+//       const oldText = this.el.innerText
+//       const length = Math.max(oldText.length, newText.length)
+//       const promise = new Promise((resolve) => this.resolve = resolve)
+//       this.queue = []
+//       for (let i = 0; i < length; i++) {
+//         const from = oldText[i] || ''
+//         const to = newText[i] || ''
+//         const start = Math.floor(Math.random() * 40)
+//         const end = start + Math.floor(Math.random() * 40)
+//         this.queue.push({ from, to, start, end })
+//       }
+//       cancelAnimationFrame(this.frameRequest)
+//       this.frame = 0
+//       this.update()
+//       return promise
+//     }
+//     update() {
+//       let output = ''
+//       let complete = 0
+//       for (let i = 0, n = this.queue.length; i < n; i++) {
+//         let { from, to, start, end, char } = this.queue[i]
+//         if (this.frame >= end) {
+//           complete++
+//           output += to
+//         } else if (this.frame >= start) {
+//           if (!char || Math.random() < 0.28) {
+//             char = this.randomChar()
+//             this.queue[i].char = char
+//           }
+//           output += `<span class="dud">${char}</span>`
+//         } else {
+//           output += from
+//         }
+//       }
+//       this.el.innerHTML = output
+//       if (complete === this.queue.length) {
+//         this.resolve()
+//       } else {
+//         this.frameRequest = requestAnimationFrame(this.update)
+//         this.frame++
+//       }
+//     }
+//     randomChar() {
+//       return this.chars[Math.floor(Math.random() * this.chars.length)]
+//     }
+//   }
+  
+// //   const phrases = [
+// //     'Neo,',
+// //     'sooner or later',
+// //     'you\'re going to realize',
+// //     'just as I did',
+// //     'that there\'s a difference',
+// //     'between knowing the path',
+// //     'and walking the path'
+// //   ]
+  
+//   const el = document.querySelector('.text')
+//   const fx = new TextScramble(el)
+  
+// //   let counter = 0
+// //   const next = () => {
+// //     fx.setText(phrases[counter]).then(() => {
+// //       setTimeout(next, 800)
+// //     })
+// //     counter = (counter + 1) % phrases.length
+// //   }
+  
+// //   next()
